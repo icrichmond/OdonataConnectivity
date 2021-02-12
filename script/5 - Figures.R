ab7b87d94f126d49cf45ae98c769688f32980d78
icrichmond <icrichmond@mun.ca>
2021-02-12 16:49:17 -0500

    species richness regression

--- script/5 - Figures.R
+++ script/5 - Figures.R
@@ -20,7 +20,7 @@ zyg$Group[42:49] = "NAT"
 ponds <- add_column(ponds, Group = "SWF")
 ponds$Group[1:8] = "NAT"
 
-#### Figure 1 - Regressions ####
+#### Figure 1 - Anisoptera Abundance Regressions ####
 mean <- ggplot(ani, aes(meanres, abundance, shape=Group))+
   geom_point(aes(group = Group), size = 2)+
   geom_smooth(aes(group=1),method="lm",se=FALSE,col="black")+
@@ -44,7 +44,19 @@ sd <- ggplot(ani, aes(sdres, abundance, shape=Group))+
 (mean / sd) + plot_layout(guides = "collect")
 ggsave("graphics/AnisopteraAbundanceRegression.jpg", dpi = 400)
 
-#### Figure 2 - Boxplot ####
+#### Figure 2 - Anisoptera Species Richness Regressions ####
+ggplot(ani, aes(meanres, speciescount, shape=Group))+
+  geom_point(aes(group = Group), size = 2)+
+  geom_smooth(aes(group=1),method="lm",se=FALSE,col="black")+
+  stat_cor(aes(meanres,speciescount,label = paste(..p.label..), group=1), label.y = 11, label.x = 48)+
+  stat_regline_equation(aes(meanres,speciescount, group=1), label.y = 12, label.x = 48)+
+  theme_classic() +
+  xlab("Mean Resistance") +
+  ylab("Anisoptera Species Richness")+
+  scale_shape_discrete(name = "Pond Type", labels = c("Natural", "Stormwater"))
+ggsave("graphics/AnisopteraSpeciesRichnessRegression.jpg", dpi=400)
+
+#### Figure 3 - Boxplot ####
 meltponds <- select(ponds, -Pond1)
 meltponds <- melt(meltponds, id.vars = "Group")
 
