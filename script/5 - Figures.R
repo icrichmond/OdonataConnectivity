14345e4ac409a3cd569837642c2d6b0f299e0c2a
icrichmond <icrichmond@mun.ca>
2021-03-01 13:52:33 -0500

    Check for collinearity

--- script/5 - Figures.R
+++ script/5 - Figures.R
@@ -24,8 +24,8 @@ ponds$Group[1:8] = "NAT"
 mean <- ggplot(ani, aes(meanres, abundance, shape=Group))+
   geom_point(aes(group = Group), size = 2)+
   geom_smooth(aes(group=1),method="lm",se=FALSE,col="black")+
-  stat_cor(aes(meanres,abundance,label = paste(..p.label..), group=1), label.y = 280, label.x = 48)+
-  stat_regline_equation(aes(meanres,abundance, group=1), label.y = 300, label.x = 48)+
+  stat_cor(aes(meanres,abundance,label = paste(..p.label..), group=1), label.y = 290, label.x = 0.09)+
+  stat_regline_equation(aes(meanres,abundance, group=1), label.y = 310, label.x = 0.09)+
   theme_classic() +
   xlab("Mean Resistance") +
   ylab("Anisoptera Abundance")+
@@ -34,8 +34,8 @@ mean <- ggplot(ani, aes(meanres, abundance, shape=Group))+
 sd <- ggplot(ani, aes(sdres, abundance, shape=Group))+
   geom_point(aes(group = Group), size = 2)+
   geom_smooth(aes(group=1),method="lm",se=FALSE,col="black")+
-  stat_cor(aes(sdres,abundance,label = paste(..p.label..), group=1), label.y = 280, label.x = 13)+
-  stat_regline_equation(aes(sdres,abundance, group=1), label.y = 300, label.x = 13)+
+  stat_cor(aes(sdres,abundance,label = paste(..p.label..), group=1), label.y = 280, label.x = 0.07)+
+  stat_regline_equation(aes(sdres,abundance, group=1), label.y = 300, label.x = 0.07)+
   theme_classic() +
   xlab("Standard Deviation Resistance") +
   ylab("Anisoptera Abundance")+
@@ -60,7 +60,7 @@ ggsave("graphics/AnisopteraSpeciesRichnessRegression.jpg", dpi=400)
 meltponds <- select(ponds, -Pond1)
 meltponds <- melt(meltponds, id.vars = "Group")
 
-levels(meltponds$variable) <- c("Mean Distance (km)", "Standard Deviation Distance (km)","Mean Resistance", "Standard Deviation Resistance")
+levels(meltponds$variable) <- c("Mean Resistance", "Standard Deviation Resistance","Number of Neighbours")
 
 ggboxplot(meltponds, x = "Group", y = "value", add = "jitter")+
   theme_classic()+
