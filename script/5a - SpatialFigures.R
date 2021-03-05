ef5a69122210790cc36cb4df901a8ab20ad2796d
icrichmond <icrichmond@mun.ca>
2021-03-05 13:34:19 -0500

    Creating study site script

--- script/5a - SpatialFigures.R
+++ script/5a - SpatialFigures.R
@@ -14,4 +14,19 @@ crs(current) <- "+proj=tmerc +lat_0=0 +lon_0=-76.5 +k=0.9999 +x_0=304800 +y_0=0 
 ponds <- add_column(ponds, Group = "SWF")
 ponds$Group[42:49] = "NAT"
 
+#### Reproject ####
+# reproject data into WGS 84 for ease of interpretation
+current <- projectRaster(current, crs="+init=epsg:4326")
+wgs84 <- st_crs('EPSG:4326')
+ponds <- st_transform(ponds, wgs84)
+
 #### Study Site Figure ####
+ss <- tm_shape(current)+
+  tm_raster(palette = "Greys", midpoint = NA, style = "cont", title = "Current") +
+tm_grid()+
+tm_shape(ponds)+
+  tm_dots(size = 0.12, shape = "Group", shapes = c(21, 24), col = "black",
+          title.shape = "Pond Type", shapes.labels = c("Natural", "Stormwater"))+
+tm_layout(legend.position = c("left", "bottom"), legend.bg.color = "white")
+
+tmap_save(ss, "graphics/currentmap.png", dpi = 450)
