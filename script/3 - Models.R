54889a25263b6b1b470b53a0ee6710d047caaa9e
icrichmond <icrichmond@mun.ca>
2021-03-05 10:47:58 -0500

    Models with mean and sd current

--- script/3 - Models.R
+++ script/3 - Models.R
@@ -12,85 +12,72 @@ zyg <- fread("input/cleaned/ZygopteraCleaned.csv")
 source("script/function - diagnosticplots.R")
 
 #### Linear Models ####
-# Use values from the 2 km buffer for Anisoptera, biologically relevant for them 
-# Use values from the 300 m buffer for Zygoptera, biologically relevant for them
-## Abundance ##
-# Anisoptera 
-ani_abun <- lm(abundance ~ mean.two,  data = ani)
+# Use values from the 2 km buffer for Anisoptera, biologically relevant 
+# Use values from the 300 m buffer for Zygoptera, biologically relevant
+
+## Anisoptera ##
+# Abundance
+ani_abun <- lm(abundance ~  mean.two + sd.two,  data = ani)
 summary(ani_abun)
 # significant
 
-ani_abun_hab <- lm(abundance ~ n , data = ani)
-summary(ani_abun_hab)
-# significant
-
-# Zygoptera 
-zyg_abun_res <- lm(abundance ~ meanres + sdres, data = zyg)
-summary(zyg_abun_res)
-# sd significant
-
-zyg_abun_hab <- lm(abundance ~ n, data = zyg)
-summary(zyg_abun_hab)
-
-## Shannon Diversity ## 
-# Anisoptera 
-ani_shann_res <- lm(shannon ~ meanres + sdres, data = ani)
-summary(ani_shann_res)
+# Shannon Diversity 
+ani_shann <- lm(shannon ~ mean.two + sd.two, data = ani)
+summary(ani_shann)
 # almost significant
+# test number of habitats?
 
-ani_shann_hab <- lm(shannon ~ n, data = ani)
-summary(ani_shann_hab)
-
-# Zygoptera 
-zyg_shann_res <- lm(shannon ~ meanres + sdres, data = zyg)
-summary(zyg_shann_res)
-# significant
-
-zyg_shann_hab <- lm(shannon ~ n, data = zyg)
-summary(zyg_shann_hab)
-
-## Species Richness ##
-# Anisoptera 
-ani_sr_res <- lm(speciescount ~ meanres + sdres, data = ani)
-summary(ani_sr_res)
-# significant
-
-ani_sr_hab <- lm(speciescount ~ n, data = ani)
-summary(ani_sr_hab)
-
-# Zygoptera 
-zyg_sr_res <- lm(speciescount ~ meanres + sdres, data = zyg)
-summary(zyg_sr_res)
-# significant
-
-zyg_sr_hab <- lm(speciescount ~ n, data = zyg)
-summary(zyg_sr_hab)
+# Species Richness
+ani_sr <- lm(speciescount ~ mean.two + sd.two, data = ani)
+summary(ani_sr)
+# mean significant
 
 # list models with names
-normalmodels <- list("Ani Abundance ~ Resistance" = ani_abun_res, "Ani Abundance ~ Neighbours" = ani_abun_hab,  
-                     "Zyg Abundance ~ Resistance" = zyg_abun_res, "Zyg Abundance ~ Neighbours" = zyg_abun_hab, 
-                     "Ani Shannon ~ Resistance" = ani_shann_res, "Ani Shannon ~ Neighbours" = ani_shann_hab, 
-                     "Zyg Shannon ~ Resistance" = zyg_shann_res, "Zyg Shannon ~ Neighbours" = zyg_shann_hab, 
-                     "Ani Sp. Rich. ~ Resistance" = ani_sr_res, "Ani Sp. Rich. ~ Neighbours" = ani_sr_hab, 
-                     "Zyg Sp. Rich. ~ Resistance" = zyg_sr_res, "Zyg Sp. Rich. ~ Neighbours" = zyg_sr_hab)
+normalmodels_ani <- list("Ani Abundance ~ Current" = ani_abun, "Ani Shannon ~ Current" = ani_shann, "Ani Sp. Rich. ~ Current" = ani_sr)
 
 # make diagnostic plots for each model 
-normalmodels_residplots <- imap(normalmodels, resid_plots)
-pdf("graphics/modeldiagnostics/normalmodels.pdf")
-normalmodels_residplots
+normalmodels_residplots_ani <- imap(normalmodels_ani, resid_plots)
+pdf("graphics/modeldiagnostics/normalmodels_ani.pdf")
+normalmodels_residplots_ani
 dev.off()
 
-# model diagnostic plots look good
-
-# significant results include Anisoptera abundance, Zygoptera abundance, 
-# Zygoptera species richness
+# model diagnostic plots look ok
 
 # write summary tables
-summ1 <- broom::tidy(ani_abun_res)
-write.csv(summ1, "output/models/AnisopteraAbundanceResistance_Summary.csv")
+summ1 <- broom::tidy(ani_abun)
+write.csv(summ1, "output/models/AnisopteraAbundanceCurrent_Summary.csv")
 
-summ2 <- broom::tidy(zyg_abun_res)
-write.csv(summ2, "output/models/ZygopteraAbundanceResistance_Summary.csv")
+summ2 <- broom::tidy(ani_shann)
+write.csv(summ2, "output/models/AnisopteraShannonCurrent_Summary.csv")
 
-summ3 <- broom::tidy(zyg_sr_res)
-write.csv(summ3, "output/models/ZygopteraSpeciesRichnessResistance_summary.csv")
+summ3 <- broom::tidy(ani_sr)
+write.csv(summ3, "output/models/AnisopteraRichnessCurrent_summary.csv")
+
+## Zygoptera ##
+# Abundance
+zyg_abun <- lm(abundance ~ mean.threeh + sd.threeh, data = zyg)
+summary(zyg_abun)
+# not significant
+
+# Shannon Diversity 
+zyg_shann <- lm(shannon ~ mean.threeh + sd.threeh, data = zyg)
+summary(zyg_shann)
+# almost significant (0.08)
+
+# Species Richness
+zyg_sr <- lm(speciescount ~ mean.threeh + sd.threeh, data = zyg)
+summary(zyg_sr)
+# mean significant
+
+# list models with names
+normalmodels_zyg <- list("Zyg Abundance ~ Current" = zyg_abun, "Zyg Shannon ~ Current" = zyg_shann, "Zyg Sp. Rich. ~ Current" = zyg_sr)
+
+# make diagnostic plots for each model 
+normalmodels_residplots_zyg <- imap(normalmodels_zyg, resid_plots)
+pdf("graphics/modeldiagnostics/normalmodels_zyg.pdf")
+normalmodels_residplots_zyg
+dev.off()
+
+# model diagnostic plots look ok
+
+# no significant results
