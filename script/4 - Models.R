8d62689e0b673665a06e982e53e4da58759faf55
icrichmond <icrichmond@mun.ca>
2021-06-28 09:37:08 -0400

    Updated models with new current map and 900 m buffer for dragons

--- script/4 - Models.R
+++ script/4 - Models.R
@@ -13,29 +13,29 @@ source("script/function - diagnosticplots.R")
 
 #### Correlations ####
 # check correlations between mean/sd and mean/hab 
-ggplot(ani, aes(x = n.two, y = mean.two)) + 
+ggplot(ani, aes(x = n.nine, y = mean.nine)) + 
   geom_point() + 
   geom_smooth(method = "lm", se = FALSE) + 
   stat_cor()
 # highly correlated
 
-ggplot(ani, aes(x = n.threeh, y = mean.threeh)) + 
+ggplot(ani, aes(x = n.three, y = mean.three)) + 
   geom_point() + 
   geom_smooth(method = "lm", se = FALSE) + 
   stat_cor()
-# correlation of 0.63, fairly correlated 
+# correlation of 0.54, fairly correlated 
 
-ggplot(ani, aes(x = mean.two, y = sd.two)) + 
+ggplot(ani, aes(x = mean.nine, y = sd.nine)) + 
   geom_point() + 
   geom_smooth(method = "lm", se = FALSE) + 
   stat_cor()
-# correlation of 0.5, OK
+# correlation of -0.21, OK
 
-ggplot(zyg, aes(x = mean.threeh, y = sd.threeh)) + 
+ggplot(zyg, aes(x = mean.three, y = sd.three)) + 
   geom_point() + 
   geom_smooth(method = "lm", se = FALSE) + 
   stat_cor()
-# correlation of 0.38, fairly correlated 
+# correlation of 0.23, OK
 
 # number of surrounding habitats and mean conductance are 
 # highly correlated
@@ -46,27 +46,29 @@ ggplot(zyg, aes(x = mean.threeh, y = sd.threeh)) +
 
 ## Anisoptera ##
 # Abundance
-ani_abun <- lm(abundance ~  mean.two,  data = ani)
+ani_abun <- lm(abundance ~  mean.nine,  data = ani)
 summary(ani_abun)
 # significant
 
-ani_abun_hab <- lm(abundance ~ n.two, data = ani)
+ani_abun_hab <- lm(abundance ~ n.nine, data = ani)
 summary(ani_abun_hab)
 # significant
 
 # Shannon Diversity 
-ani_shann <- lm(shannon ~ mean.two, data = ani)
+ani_shann <- lm(shannon ~ mean.nine, data = ani)
 summary(ani_shann)
-# almost significant
-ani_shann_hab <- lm(shannon ~ n.two, data = ani)
+# not significant
+
+ani_shann_hab <- lm(shannon ~ n.nine, data = ani)
 summary(ani_shann_hab)
 # not significant 
 
 # Species Richness
-ani_sr <- lm(speciescount ~ mean.two, data = ani)
+ani_sr <- lm(speciescount ~ mean.nine, data = ani)
 summary(ani_sr)
 # mean significant
-ani_sr_hab <- lm(speciescount ~ n.two, data = ani)
+
+ani_sr_hab <- lm(speciescount ~ n.nine, data = ani)
 summary(ani_sr_hab)
 # significant
 
@@ -87,40 +89,40 @@ dev.off()
 summ1 <- broom::tidy(ani_abun)
 write.csv(summ1, "output/models/AnisopteraAbundanceCurrent_Summary.csv")
 
-summ2 <- broom::tidy(ani_sr)
-write.csv(summ2, "output/models/AnisopteraRichnessCurrent_summary.csv")
+summ2 <- broom::tidy(ani_abun_hab)
+write.csv(summ2, "output/models/AnisopteraAbundanceHabitat_summary.csv")
 
-summ3 <- broom::tidy(ani_abun_hab)
-write.csv(summ3, "output/models/AnisopteraAbundanceHabitat_Summary.csv")
+summ3 <- broom::tidy(ani_sr)
+write.csv(summ3, "output/models/AnisopteraSpeciesRichnessCurrent_Summary.csv")
 
 summ4 <- broom::tidy(ani_sr_hab)
 write.csv(summ4, "output/models/AnisopteraSpeciesRichnessHabitat_Summary.csv")
 
 ## Zygoptera ##
 # Abundance
-zyg_abun <- lm(abundance ~ mean.threeh, data = zyg)
+zyg_abun <- lm(abundance ~ mean.three, data = zyg)
 summary(zyg_abun)
 # not significant
 
-zyg_abun_hab <- lm(abundance ~ n.threeh, data = zyg)
+zyg_abun_hab <- lm(abundance ~ n.three, data = zyg)
 summary(zyg_abun_hab)
 # not significant
 
 # Shannon Diversity 
-zyg_shann <- lm(shannon ~ mean.threeh, data = zyg)
+zyg_shann <- lm(shannon ~ mean.three, data = zyg)
 summary(zyg_shann)
-# almost significant (0.08)
+# almost significant (0.07)
 
-zyg_shann_hab <- lm(shannon ~ n.threeh, data = zyg)
+zyg_shann_hab <- lm(shannon ~ n.three, data = zyg)
 summary(zyg_shann_hab)
 # not significant
 
 # Species Richness
-zyg_sr <- lm(speciescount ~ mean.threeh, data = zyg)
+zyg_sr <- lm(speciescount ~ mean.three, data = zyg)
 summary(zyg_sr)
 # mean significant
 
-zyg_sr_hab <- lm(speciescount ~ n.threeh, data = zyg)
+zyg_sr_hab <- lm(speciescount ~ n.three, data = zyg)
 summary(zyg_sr_hab)
 # not significant
 
@@ -137,11 +139,11 @@ dev.off()
 
 # model diagnostic plots look ok
 
-summ6 <- broom::tidy(zyg_sr)
-write.csv(summ6, "output/models/ZygopteraRichnessCurrent_Summary.csv")
+summ6 <- broom::tidy(zyg_shann)
+write.csv(summ6, "output/models/ZygopteraShannonCurrent_Summary.csv")
 
-summ7 <- broom::tidy(zyg_shann)
-write.csv(summ7, "output/models/ZygopteraShannonCurrent_Summary.csv")
+summ7 <- broom::tidy(zyg_sr)
+write.csv(summ7, "output/models/ZygopteraSpeciesRichnessCurrent_Summary.csv")
 
 # IMPORTANT: sd seems to be a dummy variable - switches direction and magnitude 
 # of effect depending on what else is in the model, do not include in models. Can't 
