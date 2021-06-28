46ab63c533b43c820353be4d7d1da02177158320
icrichmond <icrichmond@mun.ca>
2021-06-28 09:40:18 -0400

    Updated current map with new resistance values

--- script/5a - SpatialFigures.R
+++ script/5a - SpatialFigures.R
@@ -6,7 +6,7 @@
 easypackages::packages("raster", "sf", "tmap", "tidyverse")
 
 #### Load Data ####
-current <- raster("input/CurrentMap.txt")
+current <- raster("input/CurrentMapRivers.asc")
 ponds <- readRDS("input/cleaned/CoorOttawa.rds")
 # assign CRS to current map
 crs(current) <- "+proj=tmerc +lat_0=0 +lon_0=-76.5 +k=0.9999 +x_0=304800 +y_0=0 +datum=NAD83 +units=m +no_defs"
