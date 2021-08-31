6956cea06f67eb36d80c81eae366e431a55db92b
icrichmond <icrichmond@mun.ca>
2021-08-31 11:55:09 -0400

    Cleaned code

--- script/6b - SupplementaryInfo4.R
+++ script/6b - SupplementaryInfo4.R
@@ -3,234 +3,167 @@
 # This script is for producing and analyzing redundancy analyses performed on community composition data
 
 #### Load Packages ####
-p <- c("vegan")
+p <- c("dplyr", "vegan")
 lapply(p, library, character.only=T)
 
 #### Load Data ####
 # odonates 
-
+AdultZygoptera <- read.csv("input/cleaned/ZygopteraCleaned.csv")
+AdultAnisoptera <- read.csv("input/cleaned/AnisopteraCleaned.csv")
 # mean current density 
-
+Current300 <- dplyr::select(AdultZygoptera, c(mean.three))
+Current900 <- dplyr::select(AdultAnisoptera, c(mean.nine))
 # number of surrounding habitats 
+Habitats300 <- dplyr::select(AdultZygoptera, c(n.three))
+Habitats900 <- dplyr::select(AdultAnisoptera, c(n.nine))
 
+# remove extra columns from odonate datasets 
+AdultZygoptera <- AdultZygoptera[,2:21]
+AdultAnisoptera <- AdultAnisoptera[,2:33]
 
-
-# Damselflies
-AdultZygoptera <- read.csv("D:/Users/maperron/Downloads/AdultZygoptera.csv", row.names=1)
-
-Current300 <- read.csv("D:/Users/maperron/Downloads/Current300.csv", row.names=1)
-
-Habitats300 <- read.csv("D:/Users/maperron/Downloads/Habitats300.csv", row.names=1)
-
-
-#Dragonflies
-
-AdultAnisoptera <- read.csv("D:/Users/maperron/Downloads/AdultAnisoptera.csv", row.names=1)
-
-Current900 <- read.csv("D:/Users/maperron/Downloads/Current900.csv", row.names=1)
-
-Habitats900 <- read.csv("D:/Users/maperron/Downloads/Habitats900.csv", row.names=1)
 
 #### Redundancy Analyses (RDA) ####
-
 ## Damselflies ---------
-
-#Tranform species matrix with Hellinger transformation
-
-spe.hel <- decostand(AdultZygoptera, method="hellinger")
-
-#Run RDA on Mean Current at 300m
-
-spe.rda <- rda(spe.hel~., data=Current300)
-summary(spe.rda, display=NULL) 
-
-#Check significance of Current300
-
-ordiR2step(rda(spe.hel~1, data=Current300), scope= formula(spe.rda), direction= "forward", R2scope=TRUE, pstep=1000)
-
-#get the R2 adjusted
-(R2adj <- RsquareAdj(spe.rda)$adj.r.squared)
-
-#test the significance of the axis and model
-anova.cca(spe.rda, step=1000)
-
-anova.cca(spe.rda, step=1000, by="axis")
+# Mean Current Density @ 300 m
+# transform species matrix with Hellinger transformation to downweight zeroes
+spe.hel.zyg <- decostand(AdultZygoptera, method="hellinger")
+# run RDA on mean current density at 300 m
+spe.rda.zyg.current <- rda(spe.hel.zyg~., data=Current300)
+summary(spe.rda.zyg.current, display=NULL) 
+# check significance of mean current density at 300 m
+ordiR2step(rda(spe.hel.zyg~1, data=Current300), scope= formula(spe.rda.zyg.current), direction= "forward", R2scope=TRUE, pstep=1000)
+# get the R2 adjusted
+(R2adj <- RsquareAdj(spe.rda.zyg.current)$adj.r.squared)
+# test the significance of the axis and model
+anova.cca(spe.rda.zyg.current, step=1000)
+anova.cca(spe.rda.zyg.current, step=1000, by="axis")
  
-
-
-
-
-
-#Zygoptera with number of surrounding habitats
-
-spe.rda1 <- rda(spe.hel~., data=Habitats300)
-summary(spe.rda1, display=NULL) 
-
-
-#Check significance of Habitat49)and Leucorrhinia proxima and L. frigida both had positiv e linear 300
-
-ordiR2step(rda(spe.hel~1, data=Habitats300), scope= formula(spe.rda1), direction= "forward", R2scope=TRUE, pstep=1000)
-
-
-#get the R2 adjusted
-(R2adj <- RsquareAdj(spe.rda1)$adj.r.squared)
-
-
+# Number of Surrounding Habitats @ 300 m
+# RDA
+spe.rda.zyg.habs <- rda(spe.hel.zyg~., data=Habitats300)
+summary(spe.rda.zyg.habs, display=NULL) 
+# check significance of Habitat49 and Leucorrhinia proxima and L. frigida both had positive linear 300
+ordiR2step(rda(spe.hel.zyg~1, data=Habitats300), scope= formula(spe.rda.zyg.habs), direction= "forward", R2scope=TRUE, pstep=1000)
+# get the R2 adjusted
+(R2adj <- RsquareAdj(spe.rda.zyg.habs)$adj.r.squared)
 #test the significance of the axis and model
-anova.cca(spe.rda1, step=1000)
-
-anova.cca(spe.rda1, step=1000, by="axis")
+anova.cca(spe.rda.zyg.habs, step=1000)
+anova.cca(spe.rda.zyg.habs, step=1000, by="axis")
                 
+
+## Dragonflies ---------
+# Mean Current Density @ 900 m
+# transform species matrix with Hellinger transformation to downweight zeroes
+spe.hel.ani <- decostand(AdultAnisoptera, method="hellinger")
+# run RDA on mean current density at 900 m
+spe.rda.ani.current <- rda(spe.hel.ani~., data=Current900)
+summary(spe.rda.ani.current, display=NULL) 
+# check significance of mean current density at 900 m
+ordiR2step(rda(spe.hel.ani~1, data=Current900), scope= formula(spe.rda.ani.current), direction= "forward", R2scope=TRUE, pstep=1000)
+# get the R2 adjusted
+(R2adj <- RsquareAdj(spe.rda.ani.current)$adj.r.squared)
+# test the significance of the axis and model
+anova.cca(spe.rda.ani.current, step=1000)
+anova.cca(spe.rda.ani.current, step=1000, by="axis")
   
-  
-
-
-## Dragonflies ------
-
-#Tranform species matrix with Hellinger transformation
-
-spe.hel <- decostand(AdultAnisoptera, method="hellinger")
-
-#Run RDA on Mean Current at 900m
-
-spe.rda <- rda(spe.hel~., data=Current900)
-summary(spe.rda, display=NULL) 
-
-
-#Check significance of Current 900
-
-ordiR2step(rda(spe.hel~1, data=Current900), scope= formula(spe.rda), direction= "forward", R2scope=TRUE, pstep=1000)
-
-
-#get the R2 adjusted
-(R2adj <- RsquareAdj(spe.rda)$adj.r.squared)
-
-
-#test the significance of the axis and model
-anova.cca(spe.rda, step=1000)
-
-anova.cca(spe.rda, step=1000, by="axis")
-  
-
-
-
-
-
-#Dragonflies with number of surrounding habitats
-
-spe.rda1 <- rda(spe.hel~., data=Habitats900)
-summary(spe.rda1, display=NULL) 
-
-
-#Check significance of Habitats at 900
-
-ordiR2step(rda(spe.hel~1, data=Habitats900), scope= formula(spe.rda1), direction= "forward", R2scope=TRUE, pstep=1000)
-
-
-#get the R2 adjusted
-(R2adj <- RsquareAdj(spe.rda1)$adj.r.squared)
-
-
-#test the significance of the axis and model
-anova.cca(spe.rda1, step=1000)
- 
-anova.cca(spe.rda1, step=1000, by="axis")
+# Number of Surrounding Habitats @ 900 m
+spe.rda.ani.habs <- rda(spe.hel.ani~., data=Habitats900)
+summary(spe.rda.ani.habs, display=NULL) 
+# check significance of surrounding habitats at 900 m
+ordiR2step(rda(spe.hel.ani~1, data=Habitats900), scope= formula(spe.rda.ani.habs), direction= "forward", R2scope=TRUE, pstep=1000)
+# get the R2 adjusted
+(R2adj <- RsquareAdj(spe.rda.ani.habs)$adj.r.squared)
+# test the significance of the axis and model
+anova.cca(spe.rda.ani.habs, step=1000)
+anova.cca(spe.rda.ani.habs, step=1000, by="axis")
             
 
-
-
-
-
 #### Plotting #### 
-#windows()
-plot(spe.rda, scaling=2, main="Triplot RDA - scaling 2", type="none", xlab=c("RDA1"), ylab=c("RDA2"), xlim=c(-1,1), ylim=c(-1.5,1.75))
-points(scores(spe.rda, display="sites", choices=c(1,2), scaling=2),
+# Damselflies / Mean Current Density @ 300 m
+jpeg("graphics/RDA/ZygopteraMeanCurrent.jpg")
+plot(spe.rda.zyg.current, scaling=2, main="Triplot RDA - scaling 2", type="none", xlab=c("RDA1"), ylab=c("RDA2"), xlim=c(-1,1), ylim=c(-1.5,1.75))
+points(scores(spe.rda.zyg.current, display="sites", choices=c(1,2), scaling=2),
        pch=21, col="black", bg="steelblue", cex=1.2)
 arrows(0,0,
-       scores(spe.rda, display="species", choices=c(1), scaling=2)*2,
-       scores(spe.rda, display="species", choices=c(2), scaling=2)*2,
+       scores(spe.rda.zyg.current, display="species", choices=c(1), scaling=2)*2,
+       scores(spe.rda.zyg.current, display="species", choices=c(2), scaling=2)*2,
        col="black",length=0)
-text(scores(spe.rda, display="species", choices=c(1), scaling=2)*2.1,
-     scores(spe.rda, display="species", choices=c(2), scaling=2)*2.1,
-     labels=rownames(scores(spe.rda, display="species", scaling=2)),
+text(scores(spe.rda.zyg.current, display="species", choices=c(1), scaling=2)*2.1,
+     scores(spe.rda.zyg.current, display="species", choices=c(2), scaling=2)*2.1,
+     labels=rownames(scores(spe.rda.zyg.current, display="species", scaling=2)),
      col="black", cex=0.8)    
 arrows(0,0,
-       scores(spe.rda, display="bp", choices=c(1), scaling=2),
-       scores(spe.rda, display="bp", choices=c(2), scaling=2),
+       scores(spe.rda.zyg.current, display="bp", choices=c(1), scaling=2),
+       scores(spe.rda.zyg.current, display="bp", choices=c(2), scaling=2),
        col="red")
-text(scores(spe.rda, display="bp", choices=c(1), scaling=2)+0.05,
-     scores(spe.rda, display="bp", choices=c(2), scaling=2)+0.05,
-     labels=rownames(scores(spe.rda, display="bp", choices=c(2), scaling=2)),
+text(scores(spe.rda.zyg.current, display="bp", choices=c(1), scaling=2)+0.05,
+     scores(spe.rda.zyg.current, display="bp", choices=c(2), scaling=2)+0.05,
+     labels=rownames(scores(spe.rda.zyg.current, display="bp", choices=c(2), scaling=2)),
      col="red", cex=1)  
-
-#PLOT
-#windows()
-plot(spe.rda1, scaling=2, main="Triplot RDA - scaling 2", type="none", xlab=c("RDA1"), ylab=c("RDA2"), xlim=c(-1,1), ylim=c(-1.5,1.75))
-points(scores(spe.rda1, display="sites", choices=c(1,2), scaling=2),
+dev.off()
+# Damselflies / Number of Surrounding Habitats @ 300 m
+jpeg("graphics/RDA/ZygopteraSurroundingHabitats.jpg")
+plot(spe.rda.zyg.habs, scaling=2, main="Triplot RDA - scaling 2", type="none", xlab=c("RDA1"), ylab=c("RDA2"), xlim=c(-1,1), ylim=c(-1.5,1.75))
+points(scores(spe.rda.zyg.habs, display="sites", choices=c(1,2), scaling=2),
        pch=21, col="black", bg="steelblue", cex=1.2)
 arrows(0,0,
-       scores(spe.rda1, display="species", choices=c(1), scaling=2)*2,
-       scores(spe.rda1, display="species", choices=c(2), scaling=2)*2,
+       scores(spe.rda.zyg.habs, display="species", choices=c(1), scaling=2)*2,
+       scores(spe.rda.zyg.habs, display="species", choices=c(2), scaling=2)*2,
        col="black",length=0)
-text(scores(spe.rda1, display="species", choices=c(1), scaling=2)*2.1,
-     scores(spe.rda1, display="species", choices=c(2), scaling=2)*2.1,
-     labels=rownames(scores(spe.rda1, display="species", scaling=2)),
+text(scores(spe.rda.zyg.habs, display="species", choices=c(1), scaling=2)*2.1,
+     scores(spe.rda.zyg.habs, display="species", choices=c(2), scaling=2)*2.1,
+     labels=rownames(scores(spe.rda.zyg.habs, display="species", scaling=2)),
      col="black", cex=0.8)    
 arrows(0,0,
-       scores(spe.rda1, display="bp", choices=c(1), scaling=2),
-       scores(spe.rda1, display="bp", choices=c(2), scaling=2),
+       scores(spe.rda.zyg.habs, display="bp", choices=c(1), scaling=2),
+       scores(spe.rda.zyg.habs, display="bp", choices=c(2), scaling=2),
        col="red")
-text(scores(spe.rda1, display="bp", choices=c(1), scaling=2)+0.05,
-     scores(spe.rda1, display="bp", choices=c(2), scaling=2)+0.05,
-     labels=rownames(scores(spe.rda1, display="bp", choices=c(2), scaling=2)),
+text(scores(spe.rda.zyg.habs, display="bp", choices=c(1), scaling=2)+0.05,
+     scores(spe.rda.zyg.habs, display="bp", choices=c(2), scaling=2)+0.05,
+     labels=rownames(scores(spe.rda.zyg.habs, display="bp", choices=c(2), scaling=2)),
      col="red", cex=1)  
-
-
-
-#PLOT
-#windows()
-plot(spe.rda, scaling=2, main="Triplot RDA - scaling 2", type="none", xlab=c("RDA1"), ylab=c("RDA2"), xlim=c(-1,1), ylim=c(-1.5,1.75))
-points(scores(spe.rda, display="sites", choices=c(1,2), scaling=2),
+dev.off()
+# Dragonflies / Mean Current Density @ 900 m
+jpeg("graphics/RDA/AnisopteraMeanCurrent.jpg")
+plot(spe.rda.ani.current, scaling=2, main="Triplot RDA - scaling 2", type="none", xlab=c("RDA1"), ylab=c("RDA2"), xlim=c(-1,1), ylim=c(-1.5,1.75))
+points(scores(spe.rda.ani.current, display="sites", choices=c(1,2), scaling=2),
        pch=21, col="black", bg="steelblue", cex=1.2)
 arrows(0,0,
-       scores(spe.rda, display="species", choices=c(1), scaling=2)*2,
-       scores(spe.rda, display="species", choices=c(2), scaling=2)*2,
+       scores(spe.rda.ani.current, display="species", choices=c(1), scaling=2)*2,
+       scores(spe.rda.ani.current, display="species", choices=c(2), scaling=2)*2,
        col="black",length=0)
-text(scores(spe.rda, display="species", choices=c(1), scaling=2)*2.1,
-     scores(spe.rda, display="species", choices=c(2), scaling=2)*2.1,
-     labels=rownames(scores(spe.rda, display="species", scaling=2)),
+text(scores(spe.rda.ani.current, display="species", choices=c(1), scaling=2)*2.1,
+     scores(spe.rda.ani.current, display="species", choices=c(2), scaling=2)*2.1,
+     labels=rownames(scores(spe.rda.ani.current, display="species", scaling=2)),
      col="black", cex=0.8)    
 arrows(0,0,
-       scores(spe.rda, display="bp", choices=c(1), scaling=2),
-       scores(spe.rda, display="bp", choices=c(2), scaling=2),
+       scores(spe.rda.ani.current, display="bp", choices=c(1), scaling=2),
+       scores(spe.rda.ani.current, display="bp", choices=c(2), scaling=2),
        col="red")
-text(scores(spe.rda, display="bp", choices=c(1), scaling=2)+0.05,
-     scores(spe.rda, display="bp", choices=c(2), scaling=2)+0.05,
-     labels=rownames(scores(spe.rda, display="bp", choices=c(2), scaling=2)),
+text(scores(spe.rda.ani.current, display="bp", choices=c(1), scaling=2)+0.05,
+     scores(spe.rda.ani.current, display="bp", choices=c(2), scaling=2)+0.05,
+     labels=rownames(scores(spe.rda.ani.current, display="bp", choices=c(2), scaling=2)),
      col="red", cex=1)  
-
-
-
-
-
-#PLOT
-#windows()
-plot(spe.rda1, scaling=2, main="Triplot RDA - scaling 2", type="none", xlab=c("RDA1"), ylab=c("RDA2"), xlim=c(-1,1), ylim=c(-1.5,1.75))
-points(scores(spe.rda1, display="sites", choices=c(1,2), scaling=2),
+dev.off()
+# Dragonflies / Number of Surrounding Habitats @ 900 m
+jpeg("graphics/RDA/AnisopteraSurroundingHabitats.jpg")
+plot(spe.rda.ani.habs, scaling=2, main="Triplot RDA - scaling 2", type="none", xlab=c("RDA1"), ylab=c("RDA2"), xlim=c(-1,1), ylim=c(-1.5,1.75))
+points(scores(spe.rda.ani.habs, display="sites", choices=c(1,2), scaling=2),
        pch=21, col="black", bg="steelblue", cex=1.2)
 arrows(0,0,
-       scores(spe.rda1, display="species", choices=c(1), scaling=2)*2,
-       scores(spe.rda1, display="species", choices=c(2), scaling=2)*2,
+       scores(spe.rda.ani.habs, display="species", choices=c(1), scaling=2)*2,
+       scores(spe.rda.ani.habs, display="species", choices=c(2), scaling=2)*2,
        col="black",length=0)
-text(scores(spe.rda1, display="species", choices=c(1), scaling=2)*2.1,
-     scores(spe.rda1, display="species", choices=c(2), scaling=2)*2.1,
-     labels=rownames(scores(spe.rda1, display="species", scaling=2)),
+text(scores(spe.rda.ani.habs, display="species", choices=c(1), scaling=2)*2.1,
+     scores(spe.rda.ani.habs, display="species", choices=c(2), scaling=2)*2.1,
+     labels=rownames(scores(spe.rda.ani.habs, display="species", scaling=2)),
      col="black", cex=0.8)    
 arrows(0,0,
-       scores(spe.rda1, display="bp", choices=c(1), scaling=2),
-       scores(spe.rda1, display="bp", choices=c(2), scaling=2),
+       scores(spe.rda.ani.habs, display="bp", choices=c(1), scaling=2),
+       scores(spe.rda.ani.habs, display="bp", choices=c(2), scaling=2),
        col="red")
-text(scores(spe.rda1, display="bp", choices=c(1), scaling=2)+0.05,
-     scores(spe.rda1, display="bp", choices=c(2), scaling=2)+0.05,
-     labels=rownames(scores(spe.rda1, display="bp", choices=c(2), scaling=2)),
+text(scores(spe.rda.ani.habs, display="bp", choices=c(1), scaling=2)+0.05,
+     scores(spe.rda.ani.habs, display="bp", choices=c(2), scaling=2)+0.05,
+     labels=rownames(scores(spe.rda.ani.habs, display="bp", choices=c(2), scaling=2)),
      col="red", cex=1)  
+dev.off()
